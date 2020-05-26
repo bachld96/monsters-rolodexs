3e550982cc366252471e16ac21674bf45ceebd25
Bách Lê Duy <bachld96@gmail.com>
2020-05-26 11:04:51 +0700

    Updates

---
+++ precache-manifest.e9af9e0a11a18dc604917b2e308cd3d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c8cb5943f2faffaae8071364d1f1e4",
+    "url": "/monsters-rolodexs/index.html"
+  },
+  {
+    "revision": "cbf7b859e0c55ed563e4",
+    "url": "/monsters-rolodexs/static/css/main.e4666254.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodexs/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodexs/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf7b859e0c55ed563e4",
+    "url": "/monsters-rolodexs/static/js/main.dd0a4b60.chunk.js"
+  },
+  {
+    "revision": "6252153b918e93c5720c",
+    "url": "/monsters-rolodexs/static/js/runtime-main.fbc27113.js"
+  }
+]);
